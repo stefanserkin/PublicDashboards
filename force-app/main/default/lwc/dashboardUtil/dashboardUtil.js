9fb645d7dbb4e368f0e6668a7b0d82883fa0c5f6
Stefan Serkin <stefanserkin@gmail.com>
2024-03-10 00:01:56 -0500

    Moved chart logic to utils component

---
+++ force-app/main/default/lwc/dashboardUtil/dashboardUtil.js
@@ -0,0 +1,155 @@
+/********************************************************
+* Chart js utilities for custom dashboard components
+********************************************************/
+
+/**
+ * @description Provides a base configuration, without any datasets, for a given chart type
+ * @param {String} chartType 
+ * @returns configuration for a chart js component
+ */
+const baseConfig = (chartType, stacked = false) => {
+    const chartConfig = {
+        type: chartType,
+        data: {
+            datasets: [],
+            labels: []
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'right'
+                }
+            }
+        }
+    };
+
+    // Add options by chart type
+    if (chartType === 'doughnut') {
+        chartConfig.options.animation = {
+             animateRotate: false,
+             animateScale: true
+        };
+    }
+    else if (chartType === 'bar') {
+        chartConfig.options.scales = {
+            xAxes: [{ 
+                stacked: stacked
+            }],
+            yAxes: [{
+                stacked: stacked,
+                beginAtZero: true, 
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        };
+    }
+    else if (chartType === 'horizontalBar') {
+        chartConfig.options.scales = {
+            xAxes: [{ 
+                stacked: stacked, 
+                beginAtZero: true, 
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }],
+            yAxes: [{
+                stacked: stacked
+            }]
+        };
+    }
+
+    return chartConfig;
+}
+
+/**
+ * Returns a data object to use in a chart.js doughnut or bar chart
+ * @param {Array} rows 
+ * @param {String} groupProperty 
+ * @param {String} valueProperty 
+ * @param {String} label 
+ * @returns data object
+ */
+const getUnstackedData = (rows, groupProperty, valueProperty, label) => {
+    const labels = [];
+    const dataset = {
+        data: [],
+        backgroundColor: [],
+        label: label
+    }
+    rows.forEach(row => {
+        dataset.data.push(row[valueProperty]);
+        dataset.backgroundColor.push(randomRGB());
+        labels.push(row[groupProperty]);
+    });
+    return {
+        labels: labels,
+        datasets: [dataset]
+    };
+}
+
+/**
+ * @description Returns a data object to use in a chart.js stacked bar chart
+ * @param {Array} rows AggregateResults array
+ * @param {String} groupingProperty 
+ * @param {String} subGroupingProperty 
+ * @param {String} countProperty 
+ * @returns data for a chart.js stacked bar chart
+ */
+const getStackedData = (rows, groupingProperty, subGroupingProperty, countProperty) => {
+    // Create sets and counts object to track unique owner/status combos
+    let groupingVals = new Set();
+    let subGroupingVals = new Set();
+    let counts = {}; // { groupingVal: { subGroupingVal: count } }
+
+    rows.forEach(row => {
+        let groupingVal = row[groupingProperty];
+        let subGroupingVal = row[subGroupingProperty];
+        let count = row[countProperty];
+        groupingVals.add(groupingVal);
+        subGroupingVals.add(subGroupingVal);
+        if (!counts[groupingVal]) {
+            counts[groupingVal] = {};
+        }
+        counts[groupingVal][subGroupingVal] = count;
+    });
+
+    // Convert to arrays to iterate with map
+    groupingVals = Array.from(groupingVals);
+    subGroupingVals = Array.from(subGroupingVals);
+
+    // Create datasets
+    let datasets = subGroupingVals.map(subGp => ({
+        label: subGp,
+        data: groupingVals.map(gp => (counts[gp] && counts[gp][subGp]) ? counts[gp][subGp] : 0),
+        backgroundColor: randomRGB(),
+        stack: 'Stack 0',
+    }));
+
+    // Return data object with labels from grouping values
+    return {
+        labels: [...groupingVals],
+        datasets: datasets
+    };
+}
+
+/**
+ * Generates random rgb value
+ * @returns RGB string - rgb(255,255,255)
+ */
+const randomRGB = () => {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+export {
+    baseConfig, 
+    getUnstackedData, 
+    getStackedData, 
+    randomRGB
+};
--- force-app/main/default/lwc/engineeringCasesDashboard/engineeringCasesDashboard.js
+++ force-app/main/default/lwc/engineeringCasesDashboard/engineeringCasesDashboard.js
@@ -1,7 +1,14 @@
 import { LightningElement, wire } from 'lwc';
 import { refreshApex } from "@salesforce/apex";
+import { 
+    baseConfig, 
+    getUnstackedData, 
+    getStackedData
+} from 'c/dashboardUtil';
 import getOpenCaseStatusCounts from '@salesforce/apex/CasesDashboardController.getOpenCaseStatusCounts';
 import getOpenCaseOwnerCounts from '@salesforce/apex/CasesDashboardController.getOpenCaseOwnerCounts';
+import getOpenCasePriorityCounts from '@salesforce/apex/CasesDashboardController.getOpenCasePriorityCounts';
+import getOpenCaseFacilityCounts from '@salesforce/apex/CasesDashboardController.getOpenCaseFacilityCounts';
 import getClosedCasesLastThirtyDays from '@salesforce/apex/CasesDashboardController.getClosedCasesLastThirtyDays';
 
 export default class EngineeringCasesDashboard extends LightningElement {
@@ -12,12 +19,14 @@ export default class EngineeringCasesDashboard extends LightningElement {
     wiredOpenCasesByStatus = [];
     wiredOpenCasesByOwner = [];
     wiredOpenCasesByPriority = [];
+    wiredOpenCasesByFacility = [];
     wiredClosedCasesByOwner = [];
 
     // Chart configurations
     openCasesByStatusConfig;
     openCasesByOwnerConfig;
     openCasesByPriorityConfig;
+    openCasesByFacilityConfig;
     closedCasesLastThirtyConfig;
 
     // Totals for metric components
@@ -61,18 +70,8 @@ export default class EngineeringCasesDashboard extends LightningElement {
         this.wiredOpenCasesByStatus = result;
         if (result.data) {
             let rows = JSON.parse(JSON.stringify(result.data));
-            const config = this.baseConfig('doughnut');
-            const dataset = {
-                data: [],
-                backgroundColor: [],
-                label: 'Open Cases'
-            }
-            rows.forEach(row => {
-                dataset.data.push(row.CaseCount);
-                dataset.backgroundColor.push(this.randomRGB());
-                config.data.labels.push(row.Status);
-            });
-            config.data.datasets.push(dataset);
+            const config = baseConfig('doughnut');
+            config.data = getUnstackedData(rows, 'Status', 'CaseCount', 'Open Cases by Status');
             this.openCasesByStatusConfig = config;
             this.calculateTotalOpenCasesByStatus(rows);
         } else if (result.error) {
@@ -114,18 +113,8 @@ export default class EngineeringCasesDashboard extends LightningElement {
         this.wiredClosedCasesByOwner = result;
         if (result.data) {
             let rows = JSON.parse(JSON.stringify(result.data));
-            const config = this.baseConfig('horizontalBar');
-            const dataset = {
-                data: [],
-                backgroundColor: [],
-                label: 'Closed Cases'
-            }
-            rows.forEach(row => {
-                dataset.data.push(row.CaseCount);
-                dataset.backgroundColor.push(this.randomRGB());
-                config.data.labels.push(row.CaseOwner);
-            });
-            config.data.datasets.push(dataset);
+            const config = baseConfig('horizontalBar');
+            config.data = getUnstackedData(rows, 'CaseOwner', 'CaseCount', 'Closed Cases');
             this.closedCasesLastThirtyConfig = config;
         } else if (result.error) {
             this.error = result.error;
@@ -141,53 +130,45 @@ export default class EngineeringCasesDashboard extends LightningElement {
         this.wiredOpenCasesByOwner = result;
         if (result.data) {
             let rows = JSON.parse(JSON.stringify(result.data));
-            
-            // Create sets and counts object to track unique owner/status combos
-            let owners = new Set();
-            let statuses = new Set();
-            let counts = {}; // { owner: { status: count } }
-
-            rows.forEach(row => {
-                let owner = row.CaseOwner;
-                let status = row.Status;
-                let count = row.CaseCount;
-        
-                owners.add(owner);
-                statuses.add(status);
-        
-                if (!counts[owner]) {
-                    counts[owner] = {};
-                }
-        
-                counts[owner][status] = count;
-            });
-
-            // Convert to arrays to iterate with map
-            owners = Array.from(owners);
-            statuses = Array.from(statuses);
-
-            // Create datasets
-            let datasets = statuses.map(status => ({
-                label: status,
-                data: owners.map(owner => (counts[owner] && counts[owner][status]) ? counts[owner][status] : 0),
-                backgroundColor: this.randomRGB(),
-                stack: 'Stack 0',
-            }));
-
             // Create chart config for stacked bar chart
-            const config = this.baseConfig('bar', true);
-            config.data.datasets = datasets;
-            config.data.labels = [...owners];
-            /*
-            config.options.scales = {
-                xAxes: { 
-                    stacked: true, 
-                    beginAtZero: true 
-                },
-                yAxes: { stacked: true }
-            }
-            */
+            const config = baseConfig('horizontalBar', true);
+            config.data = getStackedData(rows, 'CaseOwner', 'Status', 'CaseCount');
             this.openCasesByOwnerConfig = config;
+        } else if (result.error) {
+            this.error = result.error;
+            console.error(this.error);
+        }
+    }
+
+    /**
+     * @description Wire aggregate results of open cases by priority
+     */
+    @wire(getOpenCasePriorityCounts)
+    openCasesByPriorityWire(result) {
+        this.wiredOpenCasesByPriority = result;
+        if (result.data) {
+            let rows = JSON.parse(JSON.stringify(result.data));
+            // Create chart config for stacked bar chart
+            const config = baseConfig('horizontalBar', true);
+            config.data = getStackedData(rows, 'Priority', 'Status', 'CaseCount');
+            this.openCasesByPriorityConfig = config;
+        } else if (result.error) {
+            this.error = result.error;
+            console.error(this.error);
+        }
+    }
+
+    /**
+     * @description Wire aggregate results of open clases by facility name
+     */
+    @wire(getOpenCaseFacilityCounts)
+    openCasesByFacilityWire(result) {
+        this.wiredOpenCasesByFacility = result;
+        if (result.data) {
+            let rows = JSON.parse(JSON.stringify(result.data));
+            const config = baseConfig('horizontalBar');
+            config.data = getUnstackedData(rows, 'Facility', 'CaseCount', 'Open Cases');
+            this.openCasesByFacilityConfig = config;
         } else if (result.error) {
             this.error = result.error;
             console.error(this.error);
@@ -200,85 +181,9 @@ export default class EngineeringCasesDashboard extends LightningElement {
     refreshComponents() {
         refreshApex(this.wiredOpenCasesByStatus);
         refreshApex(this.wiredOpenCasesByOwner);
+        refreshApex(this.wiredOpenCasesByPriority);
+        refreshApex(this.wiredOpenCasesByFacility);
         refreshApex(this.wiredClosedCasesByOwner);
-    }
-
-    /****************************************
-     * Utilities
-     ****************************************/
-
-    /**
-     * @description Provides a base configuration, without any datasets, for a given chart type
-     * @param {String} chartType 
-     * @returns configuration for a chart js component
-     */
-    baseConfig(chartType, stacked = false) {
-        const chartConfig = {
-            type: chartType,
-            data: {
-                datasets: [],
-                labels: []
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'right'
-                    }
-                }
-            }
-        };
-
-        // Add options by chart type
-        if (chartType === 'doughnut') {
-            chartConfig.options.animation = {
-                 animateRotate: false,
-                 animateScale: true
-            };
-        }
-        else if (chartType === 'bar') {
-            chartConfig.options.scales = {
-                xAxes: [{ 
-                    stacked: stacked
-                }],
-                yAxes: [{
-                    stacked: stacked,
-                    beginAtZero: true, 
-                    ticks: {
-                        beginAtZero: true,
-                        stepSize: 1
-                    }
-                }]
-            };
-        }
-        else if (chartType === 'horizontalBar') {
-            chartConfig.options.scales = {
-                xAxes: [{ 
-                    stacked: stacked, 
-                    beginAtZero: true, 
-                    ticks: {
-                        beginAtZero: true,
-                        stepSize: 1
-                    }
-                }],
-                yAxes: [{
-                    stacked: stacked
-                }]
-            };
-        }
-
-        return chartConfig;
-    }
-
-    /**
-     * Generates random rgb value
-     * @returns RGB string - rgb(255,255,255)
-     */
-    randomRGB() {
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        return `rgb(${r},${g},${b})`;
     }
 
 }
