6a351b6e662e27bb712b4e6382250935688defa7
Stefan Serkin <stefanserkin@gmail.com>
2024-03-10 14:12:55 -0400

    Added ranges for metric component

--- force-app/main/default/lwc/dashboardMetric/dashboardMetric.js
+++ force-app/main/default/lwc/dashboardMetric/dashboardMetric.js
@@ -3,9 +3,27 @@ import { LightningElement, api } from 'lwc';
 export default class DashboardMetric extends LightningElement {
     @api title;
     @api value;
+    @api minMidRange;
+    @api minTopRange;
+    @api lessIsMore = false;
+
+    colorClasses = [
+        'low-range',
+        'mid-range',
+        'top-range'
+    ];
 
     get valueStyle() {
-        const colorStyle = this.value > 0 ? 'slds-text-color_destructive' : 'slds-text-color_success';
-        return `dash-metric-value ${colorStyle}`;
+        return `dash-metric-value ${this.colorStyle}`;
     }
+
+    get colorStyle() {
+        let ranges = [...this.colorClasses];
+        if (this.lessIsMore)
+            ranges.reverse();
+
+        const rangeIndex = this.value < this.minMidRange ? 0 : (this.value < this.minTopRange ? 1 : 2);
+        return ranges[rangeIndex];
+    }
+
 }
